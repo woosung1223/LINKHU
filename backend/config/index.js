c0d0258e4839fa509ed5c7a6c1a3ec5bc642a856
Guga <103228463+BGuga@users.noreply.github.com>
2022-10-07 17:46:12 +0900

    Configuration Setting

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,9 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    port : process.env.PORT,
-    sessionSecret : process.env.SessionSecret,
-    nodeEnv : process.env.NODE_ENV,
-    dbUrl : process.env.DB_URL,
+  port: process.env.PORT,
+  sessionSecret: process.env.SessionSecret,
+  nodeEnv: process.env.NODE_ENV,
+  dbUrl: process.env.DB_URL,
+  AwsId: process.env.AWS_ID,
+  AwsPw: process.env.AWS_SECRET,
 };
